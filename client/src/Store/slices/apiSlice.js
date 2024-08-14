e5bd64da26cd46107be54851e6dbb7980bd8c312
Sivam Manoj <151199957+Sivam-Manoj@users.noreply.github.com>
2024-08-14 20:33:36 +0530

    Update apiSlice.js

--- client/src/Store/slices/apiSlice.js
+++ client/src/Store/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 //const url = process.env.BASE_URL;
 const baseQuery = fetchBaseQuery({
-  baseUrl:  "http://13.51.172.58:8000",
+  baseUrl:  {process.env.BASE_URL},
   credentials: "include",
 });
 
